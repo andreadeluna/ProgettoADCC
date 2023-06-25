9d9180034754b5bf30ed50a05c71bd1790e6d812
Andrea De Luna <andrea.deluna10@gmail.com>
2023-06-25 19:33:25 +0200

    dht_utils: creazione funzione distance

--- src/dht_utils.erl
+++ src/dht_utils.erl
@@ -1,5 +1,9 @@
 -module(dht_utils).
--author("andreadeluna").
+-export([distance/2]).
 
-%% API
--export([]).
+distance(Id1, Id2) when is_integer(Id1), is_integer(Id2) ->
+  XorResult = Id1 bxor Id2,
+  io:format("XorResult:: ~p~n", [XorResult]),
+  BinaryResult = integer_to_binary(XorResult, 2),
+  ListResult = binary_to_list(BinaryResult),
+  length(ListResult).
