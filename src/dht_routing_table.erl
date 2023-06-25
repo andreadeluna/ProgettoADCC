0949dd6cc655c7c1500af35c56ec76bb1e2432b8
Andrea De Luna <andrea.deluna10@gmail.com>
2023-06-25 19:03:44 +0200

    dht_routing_table: creazione funzioni init e add_node

--- src/dht_routing_table.erl
+++ src/dht_routing_table.erl
@@ -1,3 +1,17 @@
 -module(dht_routing_table).
 -include("dht_routing_table.hrl").
--author("andreadeluna").
+
+-export([init/1]).
+
+-define(BUCKET_SIZE, 20).
+
+init(NodeId) ->
+  Buckets = lists:foldl(fun(_, Acc) -> [dict:new() | Acc] end, [], lists:seq(1, 160 div ?BUCKET_SIZE)),
+  #dht_routing_table{node_id = NodeId, buckets = Buckets}.
+
+add_node(#dht_routing_table{node_id = NodeId, buckets = Buckets} = Table, NodeInfo) ->
+  BucketIndex = get_bucket_index(Table, NodeId),
+  Bucket = lists:nth(BucketIndex, Buckets),
+  UpdatedBucket = replace_node(Bucket, NodeInfo),
+  UpdatedBuckets = replace_element(BucketIndex, UpdatedBucket, Buckets),
+  Table#dht_routing_table{buckets = UpdatedBuckets}.
