53b679a5b18bea8d760b9d36bb99e174ee3c1986
Andrea De Luna <andrea.deluna10@gmail.com>
2023-06-28 23:53:42 +0200

    dht_routing_table: creazione funzioni remove_node e replace_node

--- src/dht_routing_table.erl
+++ src/dht_routing_table.erl
@@ -15,3 +15,30 @@ add_node(#dht_routing_table{node_id = NodeId, buckets = Buckets} = Table, NodeIn
   UpdatedBucket = replace_node(Bucket, NodeInfo),
   UpdatedBuckets = replace_element(BucketIndex, UpdatedBucket, Buckets),
   Table#dht_routing_table{buckets = UpdatedBuckets}.
+
+remove_node(NodeId, RoutingTable) ->
+  NewBucket = remove_node_from_bucket(NodeId, RoutingTable#dht_routing_table.buckets),
+  RoutingTable#dht_routing_table{buckets = NewBucket}.
+
+remove_node_from_bucket(_, []) ->
+  [];
+remove_node_from_bucket(NodeId, [Bucket | Rest]) ->
+  NewBucket = lists:filter(fun(Node) -> Node#node_info.id /= NodeId end, Bucket),
+  [NewBucket | remove_node_from_bucket(NodeId, Rest)].
+
+replace_node(Bucket, NodeInfo) ->
+  case dict:find(NodeInfo#node_info.id, Bucket) of
+    {ok, _} ->
+      Bucket;
+    error ->
+      case dict:size(Bucket) >= ?BUCKET_SIZE of
+        true ->
+          % Il bucket è pieno, sostituisci il nodo con lo stesso ID
+          SmallestId = find_smallest_id(Bucket),
+          UpdatedBucket = dict:erase(SmallestId, Bucket),
+          dict:store(NodeInfo#node_info.id, NodeInfo, UpdatedBucket);
+        false ->
+          % Il bucket non è pieno, aggiungi il nodo
+          dict:store(NodeInfo#node_info.id, NodeInfo, Bucket)
+      end
+  end.
