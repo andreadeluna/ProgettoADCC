631b78956df1ae29b759d032e8d42c7d0e904328
Andrea De Luna <andrea.deluna10@gmail.com>
2023-06-29 00:03:07 +0200

    dht_datastore: creazione funzioni init, store e lookup

--- src/dht_datastore.erl
+++ src/dht_datastore.erl
@@ -1,3 +1,19 @@
 -module(dht_datastore).
 -include("dht_datastore.hrl").
--author("andreadeluna").
+
+-export([init/0, store/3, lookup/2]).
+
+init() ->
+  #dht_datastore{data = dict:new()}.
+
+store(Datastore, Key, Value) ->
+  UpdatedData = dict:store(Key, Value, Datastore#dht_datastore.data),
+  #dht_datastore{data = UpdatedData}.
+
+lookup(Datastore, Key) ->
+  case dict:find(Key, Datastore#dht_datastore.data) of
+    {ok, Value} ->
+      {ok, Value};
+    error ->
+      error
+  end.
