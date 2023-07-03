8e9bb46a1f56c69f82e7765262d7cf28edf8b0eb
Andrea De Luna <andrea.deluna10@gmail.com>
2023-07-03 17:08:12 +0200

    fix: aggiunta commenti al modulo dht_datastore

--- src/dht_datastore.erl
+++ src/dht_datastore.erl
@@ -1,23 +1,30 @@
+%% Modulo dht_datastore
 -module(dht_datastore).
+
+% Inclusione del record
 -include("dht_datastore.hrl").
 
+% Esportazione funzioni
 -export([
   init/0,
   store/3,
   lookup/2
 ]).
 
+%% Inizializzazione del datastore del nodo
 init() ->
   #dht_datastore{
     data = dict:new()
   }.
 
+%% Salvataggio di un valore nel datastore del nodo e aggiornamento datastore
 store(Datastore, Key, Value) ->
   UpdatedData = dict:store(Key, Value, Datastore#dht_datastore.data),
   #dht_datastore{
     data = UpdatedData
   }.
 
+%% Ricerca di un valore nel datastore di un nodo e gestione degli errori
 lookup(Datastore, Key) ->
   case dict:find(Key, Datastore#dht_datastore.data) of
     {ok, Value} ->
