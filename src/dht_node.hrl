92c9e7549780eb641db066e52dc3a7474902081c
Andrea De Luna <andrea.deluna10@gmail.com>
2023-07-03 16:43:22 +0200

    fix: aggiunta commenti file di intestazione

--- src/dht_node.hrl
+++ src/dht_node.hrl
@@ -1,3 +1,4 @@
+%% Definizione record node
 -record(node,
 {node_id,
   routing_table,
@@ -6,6 +7,7 @@
   active_nodes}
 ).
 
+%% Definizione record node_info
 -record(node_info,
 {id,
   ip,
--- src/dht_routing_table.hrl
+++ src/dht_routing_table.hrl
@@ -1,8 +1,10 @@
+%% Definizione record dht_routing_table
 -record(dht_routing_table,
 {node_id,
   buckets}
 ).
 
+%% Definizione record node_info
 -record(node_info,
 {id,
   ip,
