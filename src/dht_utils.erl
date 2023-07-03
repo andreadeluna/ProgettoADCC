0a296a3b079c70fa199ac93b6e503d30f0c2cbd5
Andrea De Luna <andrea.deluna10@gmail.com>
2023-07-03 18:53:57 +0200

    fix: aggiunta commenti al modulo dht_utils

--- src/dht_utils.erl
+++ src/dht_utils.erl
@@ -1,8 +1,12 @@
+%% Modulo dht_datastore
 -module(dht_utils).
+
+% Esportazione funzioni
 -export([
   distance/2
 ]).
 
+%% Calcolo della distanza XOR tra due nodi
 distance(Id1, Id2) when is_integer(Id1), is_integer(Id2) ->
   XorResult = Id1 bxor Id2,
   BinaryResult = integer_to_binary(XorResult, 2),
